0379491906d3f2c7049e753b6f985a1321d6127e
Christian Pojoni <dev@pojoni.at>
2022-03-20 12:51:13 +0100

    fix operator

--- auto-dl.sh
+++ auto-dl.sh
@@ -2,8 +2,8 @@
 
 while : ; do
     out=$(./swetest $@)
-    missing=$(echo $out| grep -o "SwissEph file '.*' not found in PATH" | sed -n "s/.*'\(.*\)'.*/\1/p") 
-    [[ -z "$missing" ]] || break
+    missing=$(echo "$out"| grep -o "SwissEph file '.*' not found in PATH" | sed -n "s/.*'\(.*\)'.*/\1/p") 
+    [[ -z "$missing" ]] && break
     echo "$missing"| while read -r file; do ncftpget "ftp://ftp.astro.ch/pub/swisseph/ephe/$file"; done
 done
 echo "$out"
